1a645cb4afe227546cf7b1e707ccb5760db0350a
NuanK <32317211+NuanK@users.noreply.github.com>
2017-11-23 10:04:59 +0800

    Update IMainActivity.java

--- app/src/main/java/com/bwie/a09ayjgwcdemo/view/IMainActivity.java
+++ app/src/main/java/com/bwie/a09ayjgwcdemo/view/IMainActivity.java
@@ -4,9 +4,6 @@ import com.bwie.a09ayjgwcdemo.bean.GoodsBean;
 
 import java.util.List;
 
-/**
- * Created by ASUS on 2017/11/21.
- */
 
 public interface IMainActivity {
     public void showRv(List<GoodsBean.DataBean.DatasBean>list);
